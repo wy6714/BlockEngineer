89b0154fcd00fa7992550c849cba245f2e1ba236
wy6714 <78013025+wy6714@users.noreply.github.com>
2024-04-03 19:41:18 -0400

    Plan system

---
+++ BlockEngineer/Assets/_Script/PlanOperation.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlanOperation : MonoBehaviour
+{
+    [SerializeField] private TMP_Text numText;
+    private int blockNum;
+    [SerializeField] private int cost;
+
+    private PlanSystem planSystemScript;
+
+    public static event Action<int> changeTotalFruit;
+
+    private void Awake()
+    {
+        planSystemScript = FindObjectOfType<PlanSystem>(); // Find the PlanSystem script in the scene
+    }
+
+    private void Update()
+    {
+        numText.text = blockNum.ToString();
+    }
+
+    private void Start()
+    {
+        blockNum = 0;
+    }
+
+    public void addButton()
+    {
+        if(planSystemScript.totalFruitNum >= cost)//if left fruit is enough to get block
+        {
+            blockNum += 1;
+            changeTotalFruit?.Invoke(-cost);// more block, cost furit, so minus cost
+        }
+    }
+
+    public void minusButton()
+    {
+        if (blockNum > 0)
+        {
+            blockNum -= 1;
+            changeTotalFruit?.Invoke(cost); //less block,more fruit left, so add cost
+        }
+        
+    }
+
+}
---
+++ BlockEngineer/Assets/_Script/PlanSystem.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlanSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text totalFruitText;
+    public int totalFruitNum;
+
+    private void Update()
+    {
+        totalFruitText.text = totalFruitNum.ToString();
+    }
+
+    private void OnEnable()
+    {
+        PlanOperation.changeTotalFruit += changeFruit;
+    }
+
+    private void OnDisable()
+    {
+        PlanOperation.changeTotalFruit -= changeFruit;
+    }
+
+    public void changeFruit(int cost)
+    {
+        totalFruitNum += cost;
+    }
+
+}
