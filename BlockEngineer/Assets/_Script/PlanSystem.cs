9305cb87ce951eb06ccb976720c90c84a4a66e36
wy6714 <78013025+wy6714@users.noreply.github.com>
2024-04-04 17:29:01 -0400

    view different level part

--- BlockEngineer/Assets/_Script/PlanSystem.cs
+++ BlockEngineer/Assets/_Script/PlanSystem.cs
@@ -9,7 +9,45 @@ public class PlanSystem : MonoBehaviour
     [SerializeField] private TMP_Text totalFruitText;
     public int totalFruitNum;
 
+    //view level
+    private int levelPart;
+    private float cameraMoveAmount = 20f;
+    //[SerializeField] private GameObject part1;
+    //[SerializeField] private GameObject part2;
+    //[SerializeField] private GameObject part3;
+    //[SerializeField] private GameObject part4;
+    //[SerializeField] private GameObject part5;
+    private List<GameObject> levelList = new List<GameObject>();
+
     public static event Action<int> setFinalBlockNum;
+
+    private void Start()
+    {
+        levelPart = 1;
+
+        //store all level parts
+        GameObject[] levelWithTag = GameObject.FindGameObjectsWithTag("level");
+        foreach (GameObject levelPartObj in levelWithTag)
+        {
+            levelList.Insert(0, levelPartObj);//ensure level3.1 is the first item
+        }
+        //only active level part 1 
+        foreach (GameObject levelPartObj in levelList)
+        {
+            levelPartObj.SetActive(false);
+        }
+        levelList[0].SetActive(true);
+
+        /* print items in the LevelList
+         
+        foreach (GameObject levelObject in levelList)
+        {
+            Debug.Log("Index: " + levelList.IndexOf(levelObject) + ", Name: " + levelObject.name);
+        }
+        */
+
+
+    }
 
     private void Update()
     {
@@ -34,6 +72,27 @@ public class PlanSystem : MonoBehaviour
     public void ReadyButton()
     {
         setFinalBlockNum?.Invoke(1);//int does not mean anything
+    }
+
+    public void goNext()
+    {
+        if (levelPart < 5)
+        {
+            levelList[levelPart - 1].SetActive(false);//inactive current level part
+            levelPart += 1;
+            levelList[levelPart - 1].SetActive(true);//actice next level part
+            Camera.main.transform.position += new Vector3(cameraMoveAmount,0f,0f);
+        }
+    }
+    public void goPrevious()
+    {
+        if (levelPart > 1)
+        {
+            levelList[levelPart - 1].SetActive(false);
+            levelPart -= 1;
+            levelList[levelPart - 1].SetActive(true);
+            Camera.main.transform.position += new Vector3(-cameraMoveAmount, 0f, 0f);
+        }
     }
 
 }
