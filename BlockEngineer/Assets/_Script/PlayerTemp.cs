08a8ac58148145dac510ee5ae083a7128d29e7ce
wy6714 <78013025+wy6714@users.noreply.github.com>
2024-04-15 10:59:11 -0400

    light range and temp bar

---
+++ BlockEngineer/Assets/_Script/PlayerTemp.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTemp : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float maxHealth = 100f;
+    public float healthChangeRate = 5f;
+
+    private float currentHealth;
+    private bool insideCandleLight = false;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    private void Update()
+    {
+        if (insideCandleLight)
+        {
+            IncreaseHealth(Time.deltaTime * healthChangeRate);
+        }
+        else
+        {
+            DecreaseHealth(Time.deltaTime * healthChangeRate);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("candleLight"))
+        {
+            insideCandleLight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("candleLight"))
+        {
+            insideCandleLight = false;
+        }
+    }
+
+    private void IncreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0f, maxHealth);
+        UpdateHealthBar();
+    }
+
+    private void DecreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0f, maxHealth);
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthSlider.value = currentHealth / maxHealth;
+    }
+}
